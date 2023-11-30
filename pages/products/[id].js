761bd17e6f7c0aeadf1d3c248363b29edc9f6c7b
marchfirst01 <marchfirst3113@gmail.com>
2023-11-30 17:24:54 +0900

    Codeit Mall

---
+++ pages/products/[id].js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from '@/lib/axios';
+import SizeReviewList from '@/components/SizeReviewList';
+
+const Product = () => {
+  const [product, setProduct] = useState();
+  const [sizeReviews, setSizeReviews] = useState();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const getProduct = async targetId => {
+    const res = await axios.get(`/products/${targetId}`);
+    const nextProduct = res.data;
+    setProduct(nextProduct);
+  };
+
+  const getSizeReviews = async targetId => {
+    const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
+    const nextSizeReviews = res.data.results ?? [];
+    setSizeReviews(nextSizeReviews);
+  };
+
+  useEffect(() => {
+    if (!id) return;
+
+    getProduct(id);
+    getSizeReviews(id);
+  }, [id]);
+
+  if (!product) return null;
+
+  return (
+    <>
+      <div>
+        <h1>{product.name}</h1>
+        <img src={product.imgUrl} alt={product.name} />
+        <SizeReviewList sizeReviews={sizeReviews} />
+      </div>
+    </>
+  );
+};
+
+export default Product;
